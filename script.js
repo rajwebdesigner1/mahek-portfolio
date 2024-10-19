d857fbd23a4f27ef1d5a1edd22e4f23685de2981
Rajanya Deb <145015426+rajwebdesigner1@users.noreply.github.com>
2024-10-19 22:44:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('darkModeBtn').addEventListener('click', function() {
+    document.body.className = 'dark-mode';
+});
+
+document.getElementById('lightModeBtn').addEventListener('click', function() {
+    document.body.className = 'light-mode';
+});
+
+document.getElementById('specialModeBtn').addEventListener('click', function() {
+    document.body.className = 'special-mode';
+});
